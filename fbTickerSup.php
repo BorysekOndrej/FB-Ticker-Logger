385598db46dd87972fa8f92335d2eb3664f6c0c9
Ondřej Borýsek <ondrej.borysek+github@gmail.com>
2016-02-28 17:55:54 +0100

    Striping formatting
    Striping formatting

--- fbTickerSup.php
+++ fbTickerSup.php
@@ -1,38 +1,39 @@
 <?php
+$amIDebuging = false;
+$allowedIP 	 = "1.2.3.4";
+$db_server	 = "";
+$db_username = "";
+$db_password = "";
+$db_database = "";
+$db_table	 = "";
+
+include 'privateConfig.php';
+//echo $_SERVER['REMOTE_ADDR']."<br>".$allowedIP."<br>";
+
 function homeIP(){
-	$myIP = "1.2.3.4"; // not really but, I kinda don't wanna publish test with my IP
-	if ($_SERVER['REMOTE_ADDR'] == $myIP ){ 
+	global $allowedIP;
+	if ($_SERVER['REMOTE_ADDR'] == $allowedIP ){ 
 		return true;
 	}
-/*	if ($_SERVER['HTTP_X_FORWARDED_FOR']== $myIP){ // bypass for cloudflare, but dangerous - another solution
-		return true;
-	}
-*/
 	return false;
 }
 
 
-if (!homeIP())
+if (!homeIP()){
 	die("disabled 1");
+}
 
 if ($mode!="save" && $mode!="access" && $mode!="load"){
 	die("disabled 2");
 }
 
 if (homeIP()){
-	$amIDebuging = true;
 	if ($amIDebuging){
 		ini_set("display_errors", 1);
 		error_reporting(E_ALL);    
 	}
 }
 
-
-	$db_server	 = "";
-	$db_username = "";
-	$db_password = "";
-	$db_database = "";
-	$db_table	 = "";
 
 	$connId = mysqli_connect($db_server,$db_username,$db_password) or die("Internal Error 1");
 	$selectDb = mysqli_select_db($connId, $db_database) or die("Internal Error 2");
@@ -51,14 +52,19 @@ if (homeIP()){
 	
 
 
- function cleanupString ($storyBodyOrig){
-    $searchOfset = strpos($storyBodyOrig, 'https');
-    $storyBody = substr($storyBodyOrig, 0, $searchOfset);
-    $storyBody .= 'https://blank.borysek.eu';
-    $storyBody .= substr($storyBodyOrig, strpos($storyBodyOrig, '"', $searchOfset));
+ function cleanupString ($storyBody){
+	$shortString = '';
+	$offset = 0;
+	$startText = strpos($storyBody, '<div class="tickerFeedMessage">', $offset);
+	while ( $startText > 0) {
+	    $endText = strpos($storyBody, '</div>', $startText);
+	    $offset = $endText+6;
+        $shortString .= substr($storyBody, $startText, $offset-$startText);
+        $shortString .= "\n";
+        $startText = strpos($storyBody, '<div class="tickerFeedMessage">', $offset);
+	}
 
-    $storyBody = str_replace("fbcdn.net", "blank.borysek.eu", $storyBody); //previous rule does not catch few special cases, so this saves it, although every modified request will be unique
-    return $storyBody;
+    $shortString = str_replace("fbcdn.net", "blank.borysek.eu", $shortString);
+
+	return $shortString;
 }
-
-
